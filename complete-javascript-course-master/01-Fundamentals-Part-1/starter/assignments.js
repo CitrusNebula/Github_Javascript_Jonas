85bdc34d06721efa067ac4c6ff04b2fd00e772e4
CitrusNebula <72046305+CitrusNebula@users.noreply.github.com>
2021-12-09 22:36:11 -0500

    Created Respository
    title

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,34 @@
+"Values and Variables";
+
+
+const country = "USA";
+const continent = "NorthAmerica";
+let population = 329000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+"Data Types";
+
+//let isIsland = false;
+//let language;
+
+//console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+//console.log(typeof language);
+
+
+"let, const and var"
+
+const language = "english";
+const isIsland = false;
+//isIsland = true;
+
+"Basic Operators"
+
+
+
+
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+/*
+
+let js = "amazing";
+console.log(40 + 5 + 11 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+*/
+
+/*
+
+let javascriptISFun = true;
+console.log(javascriptISFun);
+
+console.log(typeof true);
+console.log(typeof javascriptISFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptISFun = 'YES!';
+
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;

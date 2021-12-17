ae001f75de5771ecedae62d4a1bbac7bab8e6d74
CitrusNebula <72046305+CitrusNebula@users.noreply.github.com>
2021-12-17 00:29:50 -0500

    coding challenge 2
    completed

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -2,12 +2,12 @@
 
 
 const country = "USA";
-const continent = "NorthAmerica";
-let population = 329000000;
+const continent = "North America";
+let usPopulation = 330000000;
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(usPopulation);
 
 
 "Data Types";
@@ -16,19 +16,42 @@ console.log(population);
 //let language;
 
 //console.log(typeof isIsland);
-console.log(typeof population);
+console.log(typeof usPopulation);
 console.log(typeof country);
 //console.log(typeof language);
 
 
-"let, const and var"
+"let, const and var";
 
-const language = "english";
+const usLanguage = "english";
 const isIsland = false;
 //isIsland = true;
 
-"Basic Operators"
+"Basic Operators";
+
+console.log(usPopulation / 2);
+usPopulation++;
+console.log(usPopulation);
+
+const finlandPopulation = 6000000;
+console.log(usPopulation < finlandPopulation);
+const avgPopulation = 33000000;
+console.log(usPopulation < avgPopulation);
+
+const description1 = 'The USA is in ' + continent + ' and its ' + usPopulation + ' million people speak ' + usLanguage;
+console.log(description1);
 
 
 
+"Strings and Template Literals";
 
+const description2 = `The ${country} is in ${continent} and it's ${usPopulation} million people speak ${usLanguage}`;
+console.log(description2);
+
+"Taking Decisions: if/else Statements";
+
+if(usPopulation > avgPopulation){
+    console.log(`${country} population is above average`)
+} else {
+    console.log(`${country} population is ${avgPopulation - usPopulation} below average`)
+}
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -20,10 +20,70 @@ console.log(typeof 'Jonas');
 
 javascriptISFun = 'YES!';
 
-*/
-
 let age = 30;
 age = 31;
 
 const birthYear = 1991;
-// birthYear = 1990;
+birthYear = 1990;
+*/
+
+
+/*
+
+const firstName = 'Dario';
+const job = 'Sales Associate'
+const birthYear = 1991;
+const year = 2021;
+
+const dario = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job;
+console.log(dario);
+
+const darioNew = `I'm ${firstName}, a ${year -birthYear} year old ${job}`;
+console.log(darioNew);
+
+const age = 15;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license 👍🚗`)
+}  else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah has ${yearsLeft} left`)
+}
+
+*/ 
+
+"Coding Challenge 1";
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
+
+const markHigherBMI = (markBMI >= johnBMI);
+console.log(markHigherBMI);
+
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+console.log(markBMI2, johnBMI2);
+
+const markHigherBMI2 = markBMI2 >= johnBMI2;
+console.log(markHigherBMI2)
+
+
+
+"Coding Challaenge 2";
+
+if(markBMI2 > johnBMI2){
+    console.log(`Mark's BMI ${markBMI2} is higher than John's BMI ${johnBMI2}`);
+} else {
+    console.log(`John's BMI ${johnBMI2} is higher than Mark's BMI ${markBMI2}`);
+}

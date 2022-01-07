f42c00fa177aa96bc4145886d5acc80818aacee9
CitrusNebula <72046305+CitrusNebula@users.noreply.github.com>
2022-01-07 16:25:47 -0500

    JS Fundamentals - Part 2, Coding Challenge 1
    Done

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
@@ -27,7 +27,7 @@ const descFinland = describeCountry("Finland", 6, "Helsinki");
 console.log(descPortugal, descGermany, descFinland);
 console.log("", descPortugal, "\n", descGermany, "\n", descFinland);  // better readability in log  
 
-*/
+
 
 ("Declarations vs Expression");
 
@@ -48,3 +48,39 @@ const percentageUSA2 = percentageOfWorld2(330);
 const percentageCanada2 = percentageOfWorld2(38);
 const percentageFrance2 = percentageOfWorld2(67);
 console.log("", percentageUSA2, "\n", percentageCanada2, "\n", percentageFrance2);
+
+
+
+("Arrow Functions");
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const percentageUSA = console.log(percentageOfWorld3(330));
+const percentageCanada = console.log(percentageOfWorld3(38));
+const percentageFrance = console.log(percentageOfWorld3(67));
+
+("Functions Calling Other Functions");
+
+// my way
+const describePopulation = function (country, population) {
+  return `${country} has ${population} million people which is about ${percentageOfWorld3(
+    population
+  )} of the world `;
+};
+
+console.log(describePopulation("China", 1441));
+console.log(describePopulation("USA", 330));
+console.log(describePopulation("France", 67));
+
+// his way
+const describePopulationJonas = function (country, population) {
+  const percentage = percentageOfWorld3(population);
+  const description = `${country} has ${population} million
+ people, which is about ${percentage}% of the world.`;
+  console.log(description);
+};
+describePopulationJonas("Portugal", 10);
+describePopulationJonas("China", 1441);
+describePopulationJonas("USA", 332);
+
+*/
--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';  // must be very first statement to work (comments are allowed)"
+"use strict"; // must be very first statement to work (comments are allowed)"
 
 /*
 
@@ -72,5 +72,97 @@ console.log(age2);
 // in javascript functions are just values so, you can store them in variables
 
 
+
+
+("Arrow Functions");
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+}
+
+// Arrow function
+const calcAge3 = birthYear => 2021 - birthYear;
+const age3 = calcAge3(1996);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1996, 'Dario'));
+console.log(yearsUntilRetirement(1967, 'Claudette'));
+
+
+
+("Function Calling Other Functions");
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, grapes) {
+  // apples and grapes are the parameters
+  const applePieces = cutFruitPieces(apples);
+  const grapePieces = cutFruitPieces(grapes);
+  const juice = `Juice with ${applePieces} apples pieces and ${grapePieces} grape pieces`;
+  return juice;
+}
+
+console.log(fruitProcessor(2,3));
+
+
+
+("Reviewing Functions");
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  // return retirement;
+  if (retirement > 0) {
+    return `${firstName} retires in ${retirement} years`;
+    // return retirement;
+  } else {
+    return `${firstName} has been retired for ${Math.abs(retirement)} years`;
+    // return retirement;
+  }
+};
+
+const dario = yearsUntilRetirement(1996, "Dario");
+console.log(dario);
+const tom = yearsUntilRetirement(1960, "Tom");
+console.log(tom);
+
 */
 
+("Coding Challenge 1");
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const scoresDolphins = calcAverage(44, 23, 71);
+const scoresKoalas = calcAverage(65, 54, 49);
+const scoresDolphins2 = calcAverage(85, 54, 41);
+const scoresKoalas2 = calcAverage(23, 34, 27);
+
+const checkWinner = function (scoresDolphins, scoresKoalas) {
+  if (scoresDolphins >= scoresKoalas * 2) {
+    return `Dolphins win (${scoresDolphins} vs ${scoresKoalas})`;
+  } else if (scoresKoalas >= scoresDolphins * 2) {
+    return `Koalas win (${scoresKoalas} vs ${scoresDolphins})`;
+  } else if (scoresDolphins === scoresKoalas){
+    return `We have a tie!`;
+  } else {
+    return `No winner!`;
+  }
+};
+const game1 = checkWinner(scoresDolphins, scoresKoalas);
+console.log(game1);
+const game2 = checkWinner(scoresDolphins2, scoresKoalas2);
+console.log(game2);

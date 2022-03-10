20331d8ab5028718b6f8eeb4c94cc07222c0c25c
CitrusNebula <72046305+CitrusNebula@users.noreply.github.com>
2022-03-10 18:43:24 -0500

    Section 2: 45. Coding Challenge #3

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
@@ -192,3 +192,37 @@ console.log(myCountry.describe());
 console.log(myCountry.checkIsland());
 
 */
+
+
+("Coding Challenge 3");
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function(){
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function(){
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi){
+  console.log(`${mark.fullName}'s BMI ${john.bmi} is higher than ${john.fullName}'s ${mark.bmi}!`);
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI ${mark.bmi} is higher than ${mark.fullName}'s ${john.bmi}!`);
+} else {
+  console.log(`Mark and John's BMI are the same!`)
+}

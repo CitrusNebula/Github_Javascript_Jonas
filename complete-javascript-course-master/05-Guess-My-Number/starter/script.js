dbe7da629759925e8cb4b858b6085b5d1bb5f0c0
CitrusNebula <72046305+CitrusNebula@users.noreply.github.com>
2022-05-17 17:50:02 -0400

    74. Implementing the Game Logic

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -9,6 +9,35 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🤩 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '🙌 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+    if (score <= 0) {
+      document.querySelector('.message').textContent = '💥 GAME OVER! 💥';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '👇 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+    if (score <= 0) {
+      document.querySelector('.message').textContent = '💥 GAME OVER! 💥';
+    }
+  }
 });
